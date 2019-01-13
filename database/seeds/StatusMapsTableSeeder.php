ceb41208fba2368782d791e9b76ffda575dda620
ucer <185429135@qq.com>
2019-01-14 07:20:07 +0800

    update

--- database/seeds/StatusMapsTableSeeder.php
+++ database/seeds/StatusMapsTableSeeder.php
@@ -15,7 +15,12 @@ class StatusMapsTableSeeder extends Seeder
             ['table_name' => 'users', 'column' => 'enable', 'status_code' => 'T', 'status_description' => '启用', 'remark' => '是否启用'],
             ['table_name' => 'users', 'column' => 'enable', 'status_code' => 'F', 'status_description' => '禁用', 'remark' => '是否启用'],
             ['table_name' => 'users', 'column' => 'is_admin', 'status_code' => 'T', 'status_description' => '是', 'remark' => '是否为后台管理员：可登录'],
-            ['table_name' => 'users', 'column' => 'is_admin', 'status_code' => 'F', 'status_description' => '否', 'remark' => '是否为后台管理员：不可登录']
+            ['table_name' => 'users', 'column' => 'is_admin', 'status_code' => 'F', 'status_description' => '否', 'remark' => '是否为后台管理员：不可登录'],
+
+            ['table_name' => 'logs', 'column' => 'type', 'status_code' => 'insert', 'status_description' => '新增', 'remark' => '新增数据'],
+            ['table_name' => 'logs', 'column' => 'type', 'status_code' => 'update', 'status_description' => '修改', 'remark' => '修改数据'],
+            ['table_name' => 'logs', 'column' => 'type', 'status_code' => 'destroy', 'status_description' => '删除', 'remark' => '删除数据'],
+            ['table_name' => 'logs', 'column' => 'type', 'status_code' => 'error', 'status_description' => '错误日志', 'remark' => '错误日志记录'],
         ];
         \App\Models\StatusMap::insert($data);
     }
--- readme.md
+++ readme.md
@@ -2,16 +2,3 @@
 
 基于 Laravel5.7.* 与 iview3.2.1 开发的后台管理系统
 
-### 运行环境
-
-- PHP 7.1.5
-- Mysql 8.0.11
-- PHP OpenSSL 扩展
-- PHP PDO 扩展
-- PHP Mbstring 扩展
-- PHP Tokenizer 扩展
-- PHP XML 扩展
-
-
-
-### 基础安装
