2d717588a87909941ba9c64e0cb6407a1474ddd8
ucer <185429135@qq.com>
2019-02-18 16:51:11 +0800

    前端、后端解决了跨域问题

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\CorsTrait;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,7 +9,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests,CorsTrait;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function __construct()
     {
--- lu/src/libs/axios.js
+++ lu/src/libs/axios.js
@@ -4,10 +4,12 @@ import {Notice} from 'iview'
 import {getTokenFromCookies} from '@/libs/util'
 
 const addErrorLog = errorInfo => {
-  const { statusText, status,responseData, request: { responseURL } } = errorInfo
+  const {statusText, status, responseData, request: {
+      responseURL
+    }} = errorInfo
   let info = {
     type: 'ajax',
-    code: status,// http 状态码
+    code: status, // http 状态码
     message: statusText, // http 错误信息
     response_data: responseData, // http 响应头信息
     url: responseURL
@@ -17,27 +19,31 @@ const addErrorLog = errorInfo => {
 }
 
 class HttpRequest {
-  constructor (baseUrl = baseURL) {
+  constructor(baseUrl = baseURL) {
     this.baseUrl = baseUrl
     this.queue = {}
   }
-  getInsideConfig () {
+  getInsideConfig() {
     window.access_token = getTokenFromCookies()
+    let requestBaseUrl = process.env.NODE_ENV === 'development'
+      ? config.baseUrl.proxy
+      : config.baseUrl.pro
+
     const config = {
-      baseURL: this.baseUrl,
+      baseURL: requestBaseUrl,
       headers: {
         Authorization: window.access_token
       }
     }
     return config
   }
-  destroy (url) {
+  destroy(url) {
     delete this.queue[url]
     if (!Object.keys(this.queue).length) {
       // Spin.hide()
     }
   }
-  interceptors (instance, url) {
+  interceptors(instance, url) {
     // 请求拦截
     instance.interceptors.request.use(config => {
       // this.queue[url] = true
@@ -51,28 +57,42 @@ class HttpRequest {
       return res.data
     }, error => {
       this.destroy(url)
-      const { response: { statusText, status }, config, response } = JSON.parse(JSON.stringify(error))
-      const responseData = response.data  // 包含具体地错误信息
+      const {
+        response: {
+          statusText,
+          status
+        },
+        config,
+        response
+      } = JSON.parse(JSON.stringify(error))
+      const responseData = response.data // 包含具体地错误信息
 
       let errorInfo = {
         statusText,
         status,
         responseData,
-        request: { responseURL: config.url }
+        request: {
+          responseURL: config.url
+        }
       }
       if (status === 401) { // 未登录，不记录错误日志
         Cookies.remove(TOKEN_KEY)
         window.location.href = window.location.pathname + '#/login'
-      } else if (status === 412) {  // 输入验证失败，不记录错误日志
+      } else if (status === 412) { // 输入验证失败，不记录错误日志
 
       } else {
         addErrorLog(errorInfo)
       }
-      Notice.error({title: '出错了', desc: responseData?responseData.message:statusText})
+      Notice.error({
+        title: '出错了',
+        desc: responseData
+          ? responseData.message
+          : statusText
+      })
       return Promise.reject(response)
     })
   }
-  request (options) {
+  request(options) {
     const instance = axios.create()
     options = Object.assign(this.getInsideConfig(), options)
     this.interceptors(instance, options.url)
