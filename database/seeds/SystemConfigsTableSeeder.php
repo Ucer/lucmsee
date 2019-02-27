44bd5ac826065082529c71135c0f5fa68332ef4c
ucer <185429135@qq.com>
2019-02-27 14:11:01 +0800

    update

--- database/seeds/SystemConfigsTableSeeder.php
+++ database/seeds/SystemConfigsTableSeeder.php
@@ -13,7 +13,6 @@ class SystemConfigsTableSeeder extends Seeder
     {
         $data = [
             ['flag' => 'max_bak_sql_file_size', 'title' => '数据库备份每个文件最大字节(单位为M)','config_group' => 'basic','value' => '20'],
-            ['flag' => 'regex_email', 'title' => '正则邮箱验证','config_group' => 'basic','value' => '^[_.0-9a-z-]+@([0-9a-z][0-9a-z-]+.)+[a-z]{2,3}$'],
         ];
         \App\Models\Table::insert($data);
 
