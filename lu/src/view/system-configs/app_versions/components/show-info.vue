746b66b39c8263b898a6337e8ffeef2348c21696
ucer <185429135@qq.com>
2019-04-19 16:08:05 +0800

    版本详情

--- lu/src/view/system-configs/app_versions/components/show-info.vue
+++ lu/src/view/system-configs/app_versions/components/show-info.vue
@@ -1,40 +1,34 @@
 <template>
 <div>
-  <Drawer :closable="true" v-model="show" @on-close='closed' title="消息详情" :width="platformIsPc?30:80">
+  <Drawer :closable="true" v-model="show" @on-close='closed' title="版本信息" :width="platformIsPc?30:80">
     <div class="drawer-profile">
       <Row>
-        <li> <span class="blod-font">消息标题：</span> {{info.title}} </li>
-        <li v-if="info.admin_user"> <span class="blod-font">发送人：</span>{{ info.admin_user.real_name }} ({{ info.admin_user.mobile }}) </li>
-        <li v-if="info.user"><span class="blod-font"> 接收人：</span>{{ info.user.real_name }} ({{ info.user.mobile }}) </li>
+        <li> <span class="blod-font">版本号：</span> {{info.version_sn}} </li>
+        <li> <span class="blod-font">发布时间：</span> {{info.version_sn}} </li>
       </Row>
-      <h2 class="text-center margin-top-8">消息内容</h2>
-      <div class="text-center" v-html='info.content'> </div>
+      <h2 class="text-center margin-top-8">版本描述</h2>
+      <div class="text-center" v-html='info.description'> </div>
     </div>
   </Drawer>
 </div>
 </template>
 <script>
 export default {
-  props: {
-    info: {
-      type: Object,
-      default: ''
-    }
-  },
-  data() {
+  props: ['info'],
+  data () {
     return {
       show: true,
-      agreement: '',
+      agreement: ''
 
     }
   },
   computed: {
-    platformIsPc: function() {
-      return this.globalPlatformType() == 'pc' ? true : false
+    platformIsPc: function () {
+      return this.globalPlatformType() === 'pc'
     }
   },
   methods: {
-    closed() {
+    closed () {
       this.show = false
       this.$emit('show-modal-close')
     }
