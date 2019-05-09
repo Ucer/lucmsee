f2eae3d431a9c27935292da837afe14dc47a041f
ucer <185429135@qq.com>
2019-05-09 11:03:15 +0800

    doc

--- lu/src/config/index.js
+++ lu/src/config/index.js
@@ -9,7 +9,7 @@ export default {
    * @description 配置显示在浏览器标签的title
    */
   title: 'Lucmsee', // 网站标题
-  systemTitle: '我有一个家你不短简', // 菜单栏上方的大标题
+  systemTitle: 'Lucmsee', // 菜单栏上方的大标题
   copyRight: 'Lucmsee', // 请务改动 copyRight，违者必纠
   /**
    * @description token在Cookie中存储的天数，默认1天 (最好与后端 oauth_token的有效时间保持一致)
